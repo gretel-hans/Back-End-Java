346c70d5f640bfb3bcb6d1a8aaa9c99708eba7a7
gretel-hans <adjeihansel@icloud.com>
2023-05-03 09:15:13 +0200

    commit

---
+++ IntroJava/src/com/hansel/MainProject.java
@@ -0,0 +1,29 @@
+package com.hansel;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		boolean c =false;
+		char s = 'f';
+		double d = 25.3;
+		float f = 25.3f;
+		System.out.println(d);
+		
+		int array[]= new int[5];
+		
+		
+		
+		for(int i=0;i<array.length;i++) {
+			array[i]=i+1;
+		}
+		for(int i=0;i<array.length;i++) {
+		System.out.println(array[i]);	
+		}
+		
+		
+	}
+
+}
